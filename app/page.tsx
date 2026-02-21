1e9f3555c347cb19543346bba835a406b503f2d2
Cameron-774 <camgriswold2021@gmail.com>
2026-02-21 14:21:47 -0500

    Start of Website

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
 import React from "react";
-
 export default function App(){
-    <h1> is this working</h1>;
-    console.log("This should print out in the console")
-    return <div><h1> Guys we are here to win</h1></div> ;    
 
-};
+    // const videoRef = useRef<HTMLVideoElement>(null);
+    // const [cameraOpen, setCameraOpen] = useState(false);
 
+    return(
+        <div>
+            <div className="font-bold text-center text-4xl w-full bg-[#2a1a2e] py-4 px-6 flex items-center">
+                <img src="https://i.imgur.com/L29RYeB.png" height={45} width={45}/>
+                <h1>JumboVision</h1>
+            </div>
+            <div className="font-bold mt-40 px-40 text-4xl flex flex-row justify-between">
+                <div className="flex flex-col items-center">
+                    <h2>What We Do?</h2>
+                    <h3 className="text-lg max-w-[300px] text-center">To Be inserted info about what the website does</h3>
+                </div>
+                <div className="flex flex-col items-center">
+                    <h3>Start Recording</h3>
+                    <h4 className="text-lg max-w-[220px] text-center">Click Button below to start recording</h4>
+                </div>
+            </div>
+            
+        </div>
+        
+  )
+}
